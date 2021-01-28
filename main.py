44259aae6d03654b3e37b3e4a74edbeb98de6fea
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-01-29 00:30:43 +0700

    start

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-import modules.lorem as lorem
+import modules.password_cracker as password_cracker
+
+password_cracker.crack_sha1_hash('fbbe7e952d1050bfb09dfdb71d4c2ff2b3d845d2')
+
+password_cracker.crack_sha1_hash(
+    "dcc466796201f7232b22a03781110a8871fd038c", use_salts=True)
