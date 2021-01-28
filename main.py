1007420d94124415a342dad99ff74e3cd784efd1
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-01-29 01:49:27 +0700

    update: password_cracker

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import modules.password_cracker as password_cracker
 
-password_cracker.crack_sha1_hash('fbbe7e952d1050bfb09dfdb71d4c2ff2b3d845d2')
+print('use_salts=True :\n')
+password = password_cracker.crack_sha1_hash(
+    'b305921a3723cd5d70a375cd21a61e60aabb84ec')
+print(password)
 
-password_cracker.crack_sha1_hash(
-    "dcc466796201f7232b22a03781110a8871fd038c", use_salts=True)
+password = password_cracker.crack_sha1_hash(
+    'c7ab388a5ebefbf4d550652f1eb4d833e5316e3e')
+print(password)
+
+password = password_cracker.crack_sha1_hash(
+    '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8')
+print(password)
+
+password = password_cracker.crack_sha1_hash(
+    '03810a46a2c1a0eae58d9332f01c32bdcec9a01a')
+print(password)
+
+print('\nuse_salts=False :\n')
+password = password_cracker.crack_sha1_hash(
+    "53d8b3dc9d39f0184144674e310185e41a87ffd5", use_salts=True)
+print(password)
+
+password = password_cracker.crack_sha1_hash(
+    "da5a4e8cf89539e66097acd2f8af128acae2f8ae", use_salts=True)
+print(password)
--- modules/password_cracker.py
+++ modules/password_cracker.py
@@ -2,4 +2,19 @@ import hashlib
 
 
 def crack_sha1_hash(hash, use_salts=False):
-    pass
+
+    top_passwords = open('./data/top-10000-passwords.txt', 'r')
+
+    for p in top_passwords:
+        if use_salts == False:
+            if hash == hashlib.sha1(p.strip().encode()).hexdigest():
+                return p.strip()
+        else:
+            know_salts = open('./data/know-salts.txt', 'r')
+            for s in know_salts:
+                if hashlib.sha1((s.strip()+p.strip()).encode()).hexdigest() == hash:
+                    return p.strip()
+            know_salts.close()
+
+    top_passwords.close()
+    return 'PASSWORD NOT IN DATABASE'
